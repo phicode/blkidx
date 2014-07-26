38bd979490f5e74e11e8e1b676e5fa9c86b26fb6
Philipp Meinen <philipp@bind.ch>
2014-07-26 18:01:54 +0200

    main: index the current working directory

--- cmd/blkidx/main.go
+++ cmd/blkidx/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+	"os"
 
+	. "bind.ch/blkidx"
+)
+
+var (
+	flagRoot *string
+)
+
+func init() {
+	wd, err := os.Getwd()
+	if err != nil {
+		wd = ""
+	}
+	flagRoot = flag.String("root", wd, "root folder to index")
+}
+
+func main() {
+	flag.Parse()
+
+	if *flagRoot == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	var index Index = new(MemoryIndex)
+	l := log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lmicroseconds)
+	var indexer = &Indexer{
+		Index: index,
+		Log:   l,
+	}
+
+	c := WalkFiles(*flagRoot)
+	indexer.IndexAllFiles(c)
+
+	c = WalkFiles(*flagRoot)
+	indexer.IndexAllFiles(c)
 }
