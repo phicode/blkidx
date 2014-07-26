1ff7862ca7fd993842430369ecac399e7f702888
Philipp Meinen <philipp@bind.ch>
2014-07-26 15:20:40 +0200

    cmd/blkidx: add empty main command

---
+++ build/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+base=$(dirname "$0")
+base=$(readlink -e "$base")
+cd "$base"
+
+root_package="bind.ch/blkidx"
+cmd_packages="blkidx"
+
+go_get_flags="-v"
+install_flags=""
+
+if [ $# -ne 0 -a "$1" = "update" ]; then
+  # force upgrade packages
+  go_get_flags="$go_get_flags -u"
+  install_flags="$install_flags -a"
+fi
+
+rm -rf coverage
+mkdir -p coverage/{json,html,xml}
+
+echo "go-getting coverage utilities"
+go get $go_get_flags github.com/axw/gocov/gocov
+go get $go_get_flags github.com/AlekSi/gocov-xml
+go get $go_get_flags gopkg.in/matm/v1/gocov-html
+
+for pkg in $(go list "${root_package}/..."); do
+  go test -timeout=5s -coverprofile=c.out $pkg
+  if [ -e c.out ]; then
+    f=$(echo $pkg | tr '/' '_')
+    json="coverage/json/${f}.json"
+    gocov convert c.out > "$json"
+    gocov-xml  < "$json" > "coverage/xml/${f}.xml"
+    gocov-html < "$json" > "coverage/html/${f}.html"
+    rm -f c.out
+  fi
+done
+
+for cmd in $cmd_packages; do
+	go install $install_flags "${root_package}/cmd/$cmd"
+done
